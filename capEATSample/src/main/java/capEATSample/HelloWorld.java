c6fdfe0726a9e2d18dbd5c14289932effe0bec31
akilaarthy <32279268+akilaarthy@users.noreply.github.com>
2017-10-28 09:32:42 -0400

    adding servlet

--- capEATSample/src/main/java/capEATSample/HelloWorld.java
+++ capEATSample/src/main/java/capEATSample/HelloWorld.java
@@ -1,9 +1,31 @@
 package capEATSample;
 
-public class HelloWorld {
+import java.io.IOException;
 
-	public void sayHello(){
-		System.out.println("Hello");
-	}
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+
+
+public class HelloWorld  extends HttpServlet {
+
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println(" DO GET in  FC  " + req.getRequestURI());
+		//log.trace("full url: " + req.getRequestURI());
+		String requestUrl = req.getRequestURI().substring(req.getContextPath().length());
+		//log.debug("get request made with url: " + requestUrl);
+
+		// route static content normally
+		if (requestUrl.startsWith("/static")) {
+			System.out.println("In get method of front controller to direct static page");
+			super.doGet(req, resp);
+			
+		}
+	
+
+}
 }
