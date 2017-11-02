f96733f4b088c729a83d0c9528404fc4e7bcd113
Reddy <purushotham.reddy@capitalone.com>
2017-11-01 21:48:00 -0400

    Indian menu page added with all router links

--- CapEat/src/app/app.module.ts
+++ CapEat/src/app/app.module.ts
@@ -5,18 +5,25 @@ import { UIRouterModule } from '@uirouter/angular';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import {AppComponent} from './app.component';
 import {NavComponent} from './nav/nav.component';
 
 import { appRoutes } from './routes';
 import {HomeComponent} from './Home/home.component';
+import { MenuComponent } from './Menu/menu.component';
+import { MenuService } from './Menu/menu.service';
+import { StarComponent } from './shared/star.component';
+import {AddCartComponent} from './Addcart/addcart.component'
+
 
 @NgModule({
   imports: [
     BrowserModule,
     HttpModule,
     NgbModule.forRoot(),
+    HttpClientModule,
     RouterModule.forRoot(appRoutes),
     FormsModule
   ],
@@ -24,9 +31,12 @@ import {HomeComponent} from './Home/home.component';
     AppComponent,
     NavComponent,
     HomeComponent,
+    MenuComponent,
+    StarComponent,
+    AddCartComponent,
    ],
   providers: [
-
+    MenuService
    ],
   bootstrap: [AppComponent]
 })
