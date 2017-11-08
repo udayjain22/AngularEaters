357e2474a327f58b896cee152ff340181e9a5659
Reddy <purushotham.reddy@capitalone.com>
2017-11-07 21:41:15 -0500

    payment model refactor

--- UI_CapEat/src/app/routes.ts
+++ UI_CapEat/src/app/routes.ts
@@ -3,6 +3,7 @@ import { Routes } from '@angular/router';
 import { MenuComponent } from './Menu/menu.component';
 import { BewellComponent } from './Menu/bewell.component';
 import { HomeComponent } from './Home/home.component';
+import { PaymentComponent } from './payment/payment.component';
 import {AddCartComponent} from './Addcart/addcart.component';
 import {MapComponent} from './map/map.component'
 
@@ -11,6 +12,7 @@ export const appRoutes: Routes = [
   { path: 'menu', component: MenuComponent },
   { path: 'bewell', component: BewellComponent },
   { path: '', component: HomeComponent },
-  {path:'addCart/:serviceName', component: AddCartComponent},
+  {path: 'addCart/:serviceName', component: AddCartComponent},
+  {path: 'payment', component: PaymentComponent},
   {path: 'maps', component: MapComponent }
 ];
