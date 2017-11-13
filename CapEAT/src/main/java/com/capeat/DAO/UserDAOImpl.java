c6eb6ecdc6dd111c1c4244e9f5b28b7ac1c44eb1
Reddy <purushotham.reddy@capitalone.com>
2017-11-13 18:31:27 -0500

    login changes

--- CapEAT/src/main/java/com/capeat/DAO/UserDAOImpl.java
+++ CapEAT/src/main/java/com/capeat/DAO/UserDAOImpl.java
@@ -3,57 +3,57 @@ package com.capeat.DAO;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
-
 import javax.sql.DataSource;
-
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
-
+import com.capeat.beans.CardDet;
 import com.capeat.beans.UserSignIn;
-
 
 @Repository
 public class UserDAOImpl implements UserDAO {
-	
 	private JdbcTemplate jdbcTemplate;
-	
-	public UserDAOImpl(DataSource ds){
+	UserSignIn us = new UserSignIn();
+	CardDet cd = new CardDet();
+
+	public UserDAOImpl(DataSource ds) {
 		jdbcTemplate = new JdbcTemplate(ds);
 	}
 
-
-//	public JdbcTemplate getJdbcTemplate() {
-//		return jdbcTemplate;
-//	}
-
-
 	public UserDAOImpl() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
-
 
 	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
 		this.jdbcTemplate = jdbcTemplate;
 	}
 
+	@Override
+	public CardDet authentication(UserSignIn user) {
+		try {
+			String sql = ("SELECT * FROM capcafe.employee WHERE eid = \'" + user.getEid() + "\' AND password = \'"
+					+ user.getPassword() + "\'");
+			us = (UserSignIn) jdbcTemplate.queryForObject(sql, new BeanPropertyRowMapper<UserSignIn>(UserSignIn.class));
+			if (us != null) {
+				System.out.println(us.getSid());
+				return getcard();
+			}
+			return cd;
+		} catch (EmptyResultDataAccessException e) {
+			return null;
+		}
+	}
 
 	@Override
-	public List<UserSignIn> authentication(UserSignIn user) {
-		System.out.println(user.getEid());
-		System.out.println(user.getPassword());
-		String sql=("SELECT * FROM capcafe.employee WHERE eid = \'" + user.getEid() + "\' AND password = \'" + user.getPassword() + "\'");
-		
-				List<UserSignIn> uslist=jdbcTemplate.query (sql, new RowMapper<UserSignIn>(){
-					public UserSignIn mapRow(ResultSet rs,int nRows)throws SQLException{
-						UserSignIn us=new UserSignIn();						
-						us.setEid(rs.getString("eid"));
-						us.setPassword(rs.getString("password"));		
-						return us;						
-					}
-				});
-				return uslist;
+	public CardDet getcard() {
+		try {
+			String sql = ("SELECT * FROM capcafe.carddetails WHERE carddetails.sid=" + us.getSid());
+			CardDet cd = (CardDet) jdbcTemplate.queryForObject(sql, new BeanPropertyRowMapper<CardDet>(CardDet.class));
+			return cd;
+		} catch (EmptyResultDataAccessException e) {
+			return null;
 		}
-	
+	}
 }
---
+++ CapEAT/src/main/java/com/capeat/beans/CardDet.java
@@ -0,0 +1,167 @@
+package com.capeat.beans;
+
+public class CardDet {
+
+	private int cardid;
+	private String nameoncard;
+	private String cardnumber;
+	private String cardtype;
+	private String cardexpiredt;
+	private String cardcvv;
+	private String zipcode;
+	private String Status;
+
+	public CardDet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public CardDet(int cardid, String nameoncard, String cardnumber, String cardtype, String cardexpiredt,
+			String cardcvv, String zipcode, String status) {
+		super();
+		this.cardid = cardid;
+		this.nameoncard = nameoncard;
+		this.cardnumber = cardnumber;
+		this.cardtype = cardtype;
+		this.cardexpiredt = cardexpiredt;
+		this.cardcvv = cardcvv;
+		this.zipcode = zipcode;
+		Status = status;
+	}
+
+	public int getCardid() {
+		return cardid;
+	}
+
+	public void setCardid(int cardid) {
+		this.cardid = cardid;
+	}
+
+	public String getNameoncard() {
+		return nameoncard;
+	}
+
+	public void setNameoncard(String nameoncard) {
+		this.nameoncard = nameoncard;
+	}
+
+	public String getCardnumber() {
+		return cardnumber;
+	}
+
+	public void setCardnumber(String cardnumber) {
+		this.cardnumber = cardnumber;
+	}
+
+	public String getCardtype() {
+		return cardtype;
+	}
+
+	public void setCardtype(String cardtype) {
+		this.cardtype = cardtype;
+	}
+
+	public String getCardexpiredt() {
+		return cardexpiredt;
+	}
+
+	public void setCardexpiredt(String cardexpiredt) {
+		this.cardexpiredt = cardexpiredt;
+	}
+
+	public String getCardcvv() {
+		return cardcvv;
+	}
+
+	public void setCardcvv(String cardcvv) {
+		this.cardcvv = cardcvv;
+	}
+
+	public String getZipcode() {
+		return zipcode;
+	}
+
+	public void setZipcode(String zipcode) {
+		this.zipcode = zipcode;
+	}
+
+	public String getStatus() {
+		return Status;
+	}
+
+	public void setStatus(String status) {
+		Status = status;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((Status == null) ? 0 : Status.hashCode());
+		result = prime * result + ((cardcvv == null) ? 0 : cardcvv.hashCode());
+		result = prime * result + ((cardexpiredt == null) ? 0 : cardexpiredt.hashCode());
+		result = prime * result + cardid;
+		result = prime * result + ((cardnumber == null) ? 0 : cardnumber.hashCode());
+		result = prime * result + ((cardtype == null) ? 0 : cardtype.hashCode());
+		result = prime * result + ((nameoncard == null) ? 0 : nameoncard.hashCode());
+		result = prime * result + ((zipcode == null) ? 0 : zipcode.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CardDet other = (CardDet) obj;
+		if (Status == null) {
+			if (other.Status != null)
+				return false;
+		} else if (!Status.equals(other.Status))
+			return false;
+		if (cardcvv == null) {
+			if (other.cardcvv != null)
+				return false;
+		} else if (!cardcvv.equals(other.cardcvv))
+			return false;
+		if (cardexpiredt == null) {
+			if (other.cardexpiredt != null)
+				return false;
+		} else if (!cardexpiredt.equals(other.cardexpiredt))
+			return false;
+		if (cardid != other.cardid)
+			return false;
+		if (cardnumber == null) {
+			if (other.cardnumber != null)
+				return false;
+		} else if (!cardnumber.equals(other.cardnumber))
+			return false;
+		if (cardtype == null) {
+			if (other.cardtype != null)
+				return false;
+		} else if (!cardtype.equals(other.cardtype))
+			return false;
+		if (nameoncard == null) {
+			if (other.nameoncard != null)
+				return false;
+		} else if (!nameoncard.equals(other.nameoncard))
+			return false;
+		if (zipcode == null) {
+			if (other.zipcode != null)
+				return false;
+		} else if (!zipcode.equals(other.zipcode))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "CardDet [cardid=" + cardid + ", nameoncard=" + nameoncard + ", cardnumber=" + cardnumber + ", cardtype="
+				+ cardtype + ", cardexpiredt=" + cardexpiredt + ", cardcvv=" + cardcvv + ", zipcode=" + zipcode
+				+ ", Status=" + Status + "]";
+	}
+
+}
