7ba36ca17417262cb0b620244a597474e8e462c1
Uday Jain <uday.jain@capitalone.com>
2017-11-15 15:09:28 -0500

    update the pages and UI thanks

--- CapEAT/UI_CapEat/src/app/thankyou/thankyou.component.ts
+++ CapEAT/UI_CapEat/src/app/thankyou/thankyou.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./thankyou.component.css']
 })
 export class ThankyouComponent implements OnInit {
-
+public showDialog;
   constructor() { }
 
   ngOnInit() {
