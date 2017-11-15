31300f21054acd05f894b48c50139af42564f72f
Arthy <akila.arthy@capitalone.com>
2017-11-15 17:25:17 -0500

    payment

---
+++ CapEAT/UI_CapEat/src/app/Services/payments.service.ts
@@ -0,0 +1,15 @@
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs/Observable';
+import { Http } from '@angular/http';
+import { Inject } from '@angular/core';
+@Injectable()
+export class PaymentService {
+    public sendorderobj : Object;
+    
+    constructor(@Inject(Http) private http: Http) {
+    }
+    paynow(sendorderobj): Observable<any> {
+        console.log(sendorderobj);
+        return this.http.post('/CapEAT/makepayment', sendorderobj) ;
+    }
+}
