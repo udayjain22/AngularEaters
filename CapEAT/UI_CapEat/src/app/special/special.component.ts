dfea0f4d9a9a526fdd7cda8bae1569c6fb8d6b9f
Uday Jain <uday.jain@capitalone.com>
2017-11-16 23:26:31 -0500

    special page

--- CapEAT/UI_CapEat/src/app/special/special.component.ts
+++ CapEAT/UI_CapEat/src/app/special/special.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,  OnInit } from '@angular/core';
+import { RouterModule }   from '@angular/router';
 
 @Component({
   selector: 'app-special',
   templateUrl: './special.component.html',
   styleUrls: ['./special.component.css']
 })
+
 export class SpecialComponent implements OnInit {
+  title = 'special';
 
-  constructor() { }
+ constructor() {
 
-  ngOnInit() {
+
   }
 
+  ngOnInit()  {
+   
+  }
+
+ 
 }
